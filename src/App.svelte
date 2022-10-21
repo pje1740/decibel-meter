e13fffa3b483f7a8fb3bdfa3d627364ef1fce689
박소현[Papago Web] <sohyun.grace.park@navercorp.com>
2022-10-21 16:59:18 +0900

    chore   : repo initial settings

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Title from '@/components/Title.svelte'
+</script>
+
+<main>
+	<Title />
+</main>
+
+<style global>
+	@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
+
+	* {
+		font-family: 'Noto Sans KR', sans-serif;
+	}
+
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/Title.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    // your script goes here
+</script>
+
+<h1>Decibel Meter</h1>
+
+<style>
+    h1 {
+		color: black;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 900;
+	}
+</style>
+
+<!-- markup (zero or more items) goes here -->
