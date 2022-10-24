2ea37f514eec4a4769a869827631e25dd6167cd8
Sohyun Park <pje1740@gmail.com>
2022-10-24 11:07:30 +0900

    feat    : create basic layout

--- src/components/SoundMeter.svelte
+++ src/components/SoundMeter.svelte
@@ -1,14 +1,27 @@
 <script lang="ts">
 </script>
 
-<div />
+<div class="wrapper">
+	<div class="wave" />
+</div>
 
 <style>
-	div {
+	div.wrapper {
+		position: relative;
 		width: 240px;
 		height: 240px;
+		margin: 2rem;
 		background: #ffffff;
 		border: 3px solid #888888;
 		box-shadow: 0px 4px 4px 2px rgba(0, 0, 0, 0.25);
+		border-radius: 50%;
+
+		overflow: hidden;
+	}
+
+	.wave {
+		background-color: lightgray;
+		height: 100%;
+		width: 100%;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,49 @@
 <script lang="ts">
 	import SoundMeter from '@/components/SoundMeter.svelte';
+
+	let isRecording = false;
+
+	const toggleRecording = () => {
+		isRecording = !isRecording;
+	};
 </script>
 
 <main>
-	<h1>Decibel Meter</h1>
+	<h1>DECIBEL</h1>
+	<h1>METER</h1>
 	<SoundMeter />
+	<button class="main-button" on:click={toggleRecording}>{isRecording ? 'Stop' : 'Start'}</button>
 </main>
+
+<style>
+	h1 {
+		font-size: 3rem;
+		margin: 0;
+	}
+	main {
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+	button.main-button {
+		width: 100px;
+		height: 40px;
+		margin: 2rem;
+
+		background: #888888;
+		box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+		border-radius: 10px;
+		border: none;
+
+		font-style: normal;
+		font-weight: 700;
+		font-size: 16px;
+		line-height: 23px;
+		text-align: center;
+
+		color: white;
+		cursor: pointer;
+	}
+</style>
