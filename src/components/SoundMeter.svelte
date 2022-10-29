908db1bf7245fcf1591a2da4cbfb69c4b305973c
Sohyun Park <pje1740@gmail.com>
2022-10-29 12:26:00 +0900

    feat    : make meter gauge move in accordance to a value

--- src/components/SoundMeter.svelte
+++ src/components/SoundMeter.svelte
@@ -3,15 +3,21 @@
 	import { startRecording } from '@/utils/decibel';
 	import { onMount } from 'svelte';
 
+	export let volume = 0;
+	$: bottom = `${volume}px`;
+
+	const handleVolumeChange = (vol: number) => {
+		volume = vol;
+		console.log(volume);
+	};
+
 	onMount(async () => {
-		if (browser) {
-			await startRecording();
-		}
+		await startRecording(handleVolumeChange);
 	});
 </script>
 
 <div class="wrapper">
-	<div class="wave" />
+	<div class="wave" style:bottom />
 </div>
 
 <style>
@@ -21,16 +27,20 @@
 		height: 240px;
 		margin: 2rem;
 		background: #ffffff;
-		border: 3px solid #888888;
+		background-color: lightgray;
 		box-shadow: 0px 4px 4px 2px rgba(0, 0, 0, 0.25);
 		border-radius: 50%;
+		outline: 4px solid white;
+		outline-offset: -4px;
 
 		overflow: hidden;
 	}
 
 	.wave {
-		background-color: lightgray;
+		position: relative;
+		background-color: white;
 		height: 100%;
 		width: 100%;
+		transition: bottom 0.1s;
 	}
 </style>
--- src/utils/decibel.ts
+++ src/utils/decibel.ts
@@ -1,4 +1,4 @@
-export const startRecording = async () => {
+export const startRecording = async (onReceiveVolume: (volume: number) => void) => {
 	const stream = await createStream();
 
 	const audioCtx = new window.AudioContext();
@@ -13,16 +13,17 @@ export const startRecording = async () => {
 	input.connect(analyserNode);
 
 	const process = () => {
-		setInterval(() => {
-			analyserNode.getFloatFrequencyData(dataArray);
-			for (let i = 0; i < bufferLength; i++) {
-				const value = dataArray[i];
+		requestAnimationFrame(process);
 
-				//need to convert db Value because it is -120 to 0
-				const barHeight = (value / 2 + 70) * 10;
-				console.log(barHeight);
-			}
-		}, 100);
+		analyserNode.getFloatFrequencyData(dataArray);
+		for (let i = 0; i < bufferLength; i++) {
+			// TODO: 정확한 데시벨 값 측정 필요
+			const value = dataArray[i];
+			let volume = Math.floor((value + 140) * 2);
+			if (volume < 0) volume = 0;
+
+			onReceiveVolume(volume);
+		}
 	};
 
 	process();
